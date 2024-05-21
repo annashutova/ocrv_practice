86249ada62d302a709e0815e252c3131a650a199
annashutova <annashut@inbox.ru>
2024-05-21 19:37:43 +0300

    added whisper model transcription

--- parse_video.py
+++ parse_video.py
@@ -1,12 +1,17 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import pathlib
+import whisper
 
 
 CURRENT_DIR = pathlib.Path(__file__).parent.resolve()
 
 MIN_SEGMENT_DURATION = 1 * 1000  # 1 second
 MAX_SEGMENT_DURATION = 10 * 1000  # 10 seconds
+MIN_SILENCE_LEN = 500  # 0.5 seconds
+SILENCE_THRESH = -20
+# SILENCE_THRESH_DW = -35
+# MIN_SILENCE_LEN_DW = 300
 
 
 def segment_audio_file(path_to_file: str, file_format: str) -> None:
@@ -26,19 +31,30 @@ def segment_audio_file(path_to_file: str, file_format: str) -> None:
     # Split the audio on silence
     chunks: list[AudioSegment] = split_on_silence(
         mp4_audio,
-        min_silence_len=500,
-        silence_thresh=-25,
+        min_silence_len=MIN_SILENCE_LEN,
+        silence_thresh=SILENCE_THRESH,
     )
 
     # filtered_chunks = list(filter(is_filtered_by_duration, chunks))
+
+    # Load the Whisper model
+    model = whisper.load_model("base")
 
     # Export each filtered by length audio
     for i, chunk in enumerate(chunks):
         if is_filtered_by_duration(chunk):
             print(f"Chunk length: {len(chunk)} ms")
-            output_file = f"{CURRENT_DIR}/audio_segments/chunk{i}.mp3"
-            print("Exporting file", output_file)
-            chunk.export(output_file, format="mp3")
+            output_mp3_file = f"{CURRENT_DIR}/audio_segments/chunk{i}.mp3"
+            print("Exporting file", output_mp3_file)
+            chunk.export(output_mp3_file, format="mp3")
+
+            # Transcribe audio file and export
+            transcription = model.transcribe(output_mp3_file, language='ru')
+            output_txt_file = f"{CURRENT_DIR}/text_segments/chunk{i}.txt"
+
+            with open(output_txt_file, "w") as f:
+                print(f"▼ Transcription of {output_txt_file}\n")
+                f.write(transcription['text'])
 
 
 def is_filtered_by_duration(audio_segment: AudioSegment) -> bool:
