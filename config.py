30015074320343deb882a3d2aee6e1a049406b7c
annashutova <annashut@inbox.ru>
2024-05-23 18:16:18 +0300

    added transcription config

---
+++ config.py
@@ -0,0 +1,41 @@
+# friends_audio
+MIN_SEG_DUR_FRIENDS = 1 * 1000  # 1 second
+MAX_SEG_DUR_FRIENDS = 10 * 1000  # 10 seconds
+MIN_SILENCE_LEN_FRIENDS = 500  # 0.5 seconds
+SILENCE_THRESH_FRIENDS = -20
+
+# drunk_woman
+SILENCE_THRESH_DW = -40
+MIN_SILENCE_LEN_DW = 100
+
+# Переговоры_диспетчеров_с_пьяным_машинистом_в_московском_метро_на
+SILENCE_THRESH_DRIVER = -50  # perfect
+MIN_SILENCE_LEN_DRIVER = 100
+
+# Пьяные_разговоры
+SILENCE_THRESH_TALK = -55
+MIN_SILENCE_LEN_TALK = 100
+
+# Разговор_пьяного_мужа
+SILENCE_THRESH_HUSBAND = -40  # perfect
+MIN_SILENCE_LEN_HUSBAND = 200
+
+# Умный_домофон_не_пускает_домой_пьяного
+SILENCE_THRESH_DOOR = -30  # perfect
+MIN_SILENCE_LEN_DOOR = 200
+
+# russian_memes
+MIN_SEG_DUR_RM = 1 * 1000  # 1 second
+MAX_SEG_DUR_RM = 25 * 1000  # 25 seconds
+SILENCE_THRESH_RM = -40  # good
+MIN_SILENCE_LEN_RM = 200
+
+# alco_videos_1
+SILENCE_THRESH_AV1 = -40  # perfect
+MIN_SILENCE_LEN_AV1 = 200
+
+# alco_videos_1
+MIN_SEG_DUR_AV2 = 0.5 * 1000  # 0.5 second
+MAX_SEG_DUR_AV2 = 10 * 1000  # 10 seconds
+SILENCE_THRESH_AV2 = -35  # perfect
+MIN_SILENCE_LEN_AV2 = 100
