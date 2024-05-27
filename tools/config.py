391bc460f8680526217c2744f49afe472a6279a2
annashutova <annashut@inbox.ru>
2024-05-27 15:28:30 +0300

    changed project structure, added documentation

---
+++ tools/config.py
@@ -0,0 +1,65 @@
+# friends_audio
+MIN_SEG_DUR_DEFAULT = 1 * 1000  # 1 second
+MAX_SEG_DUR_DEFAULT = 10 * 1000  # 10 seconds
+MIN_SILENCE_LEN_FRIENDS = 500  # 0.5 seconds
+SILENCE_THRESH_FRIENDS = -20
+
+# drunk_woman
+SILENCE_THRESH_DW = -40
+MIN_SILENCE_LEN_DW = 100
+
+# Переговоры_диспетчеров_с_пьяным_машинистом_в_московском_метро_на
+SILENCE_THRESH_DRIVER = -50  # perfect
+MIN_SILENCE_LEN_DRIVER = 100
+
+# Пьяные_разговоры
+SILENCE_THRESH_TALK = -55
+MIN_SILENCE_LEN_TALK = 100
+
+# Разговор_пьяного_мужа
+SILENCE_THRESH_HUSBAND = -40  # perfect
+MIN_SILENCE_LEN_HUSBAND = 200
+
+# Умный_домофон_не_пускает_домой_пьяного
+SILENCE_THRESH_DOOR = -30  # perfect
+MIN_SILENCE_LEN_DOOR = 200
+
+# russian_memes
+MIN_SEG_DUR_RM = 1 * 1000  # 1 second
+MAX_SEG_DUR_RM = 25 * 1000  # 25 seconds
+SILENCE_THRESH_RM = -40  # good
+MIN_SILENCE_LEN_RM = 200
+
+# alco_videos_1
+SILENCE_THRESH_AV1 = -40  # perfect
+MIN_SILENCE_LEN_AV1 = 200
+
+# alco_videos_2
+MIN_SEG_DUR_AV2 = 0.5 * 1000  # 0.5 second
+MAX_SEG_DUR_AV2 = 10 * 1000  # 10 seconds
+SILENCE_THRESH_AV2 = -35  # perfect
+MIN_SILENCE_LEN_AV2 = 100
+
+# alco_videos_3
+SILENCE_THRESH_AV3 = -35
+MIN_SILENCE_LEN_AV3 = 100
+
+# alco_videos_4
+SILENCE_THRESH_AV4 = -40  # perfect
+MIN_SILENCE_LEN_AV4 = 100
+
+# Пьяные_сцены_из_советских_комедий
+SILENCE_THRESH_SC = -45
+MIN_SILENCE_LEN_SC = 100
+
+# yes_man
+SILENCE_THRESH_YM = -30
+MIN_SILENCE_LEN_YM = 100
+
+# купе_номер_6
+SILENCE_THRESH_6 = -20
+MIN_SILENCE_LEN_6 = 100
+
+# славик_и_димон
+SILENCE_THRESH_SD = -40
+MIN_SILENCE_LEN_SD = 100
---
+++ tools/split_video.py
@@ -0,0 +1,99 @@
+import os
+
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+import config
+
+
+BASE_DIR = os.path.dirname(os.getcwd())
+EXPORT_AUDIO_FORMAT = 'wav'
+
+
+def segment_audio_file(
+        path_to_file: str,
+        file_format: str,
+        export_audio_format: str,
+        min_seg_dur: int = 1000,
+        max_seg_dur: int = 10000,
+        min_silence_len: int = 1000,
+        silence_thresh: int = -30
+) -> None:
+    """
+    Segments an audio file into chunks based on silence and export to separate mp3 files.
+
+    Args:
+        path_to_file (str): The path to the audio file without the file extension.
+        file_format (str): The format of the audio file (e.g., 'mp4').
+        export_audio_format (str): The format of the export file (e.g., 'wav').
+        min_seg_dur (int): Minimal segment duration.
+        max_seg_dur (int): Maximum segment duration.
+        min_silence_len (int): Minimal silence duration.
+        silence_thresh (int): The upper bound for how quiet is silent in dBFS.
+
+    Returns:
+        None
+    """
+    base_export_name = os.path.basename(path_to_file)
+
+    # Load the audio file
+    mp4_audio = AudioSegment.from_file(f'{path_to_file}.{file_format}', format=file_format)
+
+    # Split the audio on silence
+    chunks: list[AudioSegment] = split_on_silence(
+        mp4_audio,
+        min_silence_len=min_silence_len,
+        silence_thresh=silence_thresh,
+        keep_silence=300  # 0.3 seconds
+    )
+
+    # Check the existence of directories
+    audio_export_dir = f'{BASE_DIR}/raw_data/audio/{base_export_name}'
+    txt_export_dir = f'{BASE_DIR}/raw_data/text/{base_export_name}'
+
+    if not os.path.exists(audio_export_dir):
+        os.mkdir(audio_export_dir)
+    if not os.path.exists(txt_export_dir):
+        os.mkdir(txt_export_dir)
+
+    # Export each filtered by length audio
+    for i, chunk in enumerate(chunks):
+        if is_filtered_by_duration(chunk, min_seg_dur, max_seg_dur):
+            print(f"Chunk length: {len(chunk)} ms")
+            output_audio_file = f"{audio_export_dir}/{base_export_name}_chunk{i}.{export_audio_format}"
+            print("Exporting file", output_audio_file)
+            chunk.export(output_audio_file, format=export_audio_format)
+
+
+def is_filtered_by_duration(
+        audio_segment: AudioSegment,
+        min_seg_dur: int = 1000,
+        max_seg_dur: int = 10000,
+) -> bool:
+    """
+    Checks whether AudioSegment duration lies between MIN_SEGMENT_DURATION and MAX_SEGMENT_DURATION.
+
+    Args:
+        audio_segment (AudioSegment): AudioSegment to check.
+        min_seg_dur (int): Minimal segment duration.
+        max_seg_dur (int): Maximum segment duration.
+
+    Returns:
+        bool
+    """
+    return min_seg_dur <= len(audio_segment) <= max_seg_dur
+
+
+if __name__ == '__main__':
+    test_file_path = f'{BASE_DIR}/raw_data/video/славик_и_димон'
+    file_format = 'mp4'
+
+    segment_audio_file(
+        test_file_path,
+        file_format,
+        EXPORT_AUDIO_FORMAT,
+        min_seg_dur=config.MIN_SEG_DUR_DEFAULT,
+        max_seg_dur=config.MAX_SEG_DUR_DEFAULT,
+        min_silence_len=config.MIN_SILENCE_LEN_SD,
+        silence_thresh=config.SILENCE_THRESH_SD,
+    )
